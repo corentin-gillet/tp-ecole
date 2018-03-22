279649388b523318815f319bb23ca8ee4c2e4133
Rilet <corentingillet65@gmail.com>
2018-03-22 13:34:49 +0100

    ajax

--- src/app.js
+++ src/app.js
@@ -3,17 +3,22 @@ $(document).ready(function () {
     $('#terminal').keypress(function (e) {
         if (e.which === 13) {
 
+
+
             $.ajax({
                 url: 'http://tp-ecole.local/src/response.php',
                 type: 'GET',
                 DataType: 'Json',
+                data: '',
 
                 success: function (data, statut) {
-                    var val = $('input').val();
-                    console.log(val);
-                    tpl.append(val)
+                    var val = $('input');
+                    tpl.append('<p class="text-center">' + val.val() +'</p>');
+                    val.val('');
+
                 },
                 error: function (data, statut, erreur) {
+
                 },
                 complete: function (data, statut) {
 
